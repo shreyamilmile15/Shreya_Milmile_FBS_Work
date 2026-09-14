690086433aeacfe8336d984475df2a65dfd2c5aa
Shreya Milmile <shreyamilmile44@gmail.com>
2026-09-15 00:58:57 +0530

    Add files via upload

---
+++ Assignment 6/q1e.py
@@ -0,0 +1,19 @@
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(' ',end =' ')
+    for j in range(1, i+1):
+        print('*',end=' ')
+    for j in range(1,i):
+        print('*',end=' ')
+    print()
+
+
+#for i in range(1, 6):
+
+#   for j in range(1, 6 - i):
+#       print(" ", end=" ")
+
+#   for j in range(1, 2 * i):
+#       print("*", end=" ")
+
+#   print()
