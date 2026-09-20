0898b654a051d594f94a95a6c359f4e3e08fc058
Shreya Milmile <shreyamilmile44@gmail.com>
2026-09-20 16:32:04 +0530

    Delete test 3 directory

--- test 3/Tests/Test 2/q4.py
+++
@@ -1,8 +0,0 @@
-#Write a program to calculate the total cost of painting. The interior of building with four equal sized walls. 
-l = int(input('enter length of wall:'))
-h = int(input('enter height of wall:'))
-rate = int(input('enter painting rate:'))
-area = 4*l*h
-cost = area * rate
-print('total area', area,'sq. m')
-print('total painting cost:',cost,'rs')
